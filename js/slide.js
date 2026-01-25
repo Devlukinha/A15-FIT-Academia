4766cb803976f12a387e88c99fcaeedd7c70332d
Devlukinnha <lucashenrique030407@gmail.com>
2026-01-25 19:33:42 -0300

    Responsividade aplicada

--- js/slide.js
+++ js/slide.js
@@ -3,7 +3,7 @@ const slides = track.children;
 const totalSlides = slides.length; 
 
 let indexSlide = 0;
-const larguraSlide = 330; 
+let larguraSlide = 330; 
 
 setInterval(() => {
 
@@ -11,6 +11,13 @@ setInterval(() => {
 
     if(indexSlide >=totalSlides) {
         indexSlide = 0;
+    }
+
+    if(window.innerWidth >= 672) {
+        larguraSlide = 430;
+    }
+    else {
+        larguraSlide = 330;
     }
 
     track.style.transform = `translateX(-${indexSlide * larguraSlide}px)`
